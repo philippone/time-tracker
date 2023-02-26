59d813a6f788c385819b3ba4949cffb41b9ee4c7
Philipp Schardt <philipp.schardt@ergosign.de>
2023-02-26 17:34:27 +0100

    initial test to load active window info

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,31 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+use active_win_pos_rs::get_active_window;
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+
+    match get_active_window() {
+        Ok(active_window) => {
+            println!("active window: {:#?}", active_window);
+            format!("active window: {:#?}", active_window);
+        
+        },
+        Err(()) => {
+            println!("error occurred while getting the active window");
+            format!("error occurred while getting the active window");
+        }
+    }
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}

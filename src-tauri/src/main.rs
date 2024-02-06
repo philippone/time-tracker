996fb8e491e4310c43e51ff1078d07ac8e58fe31
Philipp Schardt <philipp.schardt@ergosign.de>
2024-02-06 19:26:49 +0100

    save

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,19 +8,17 @@ use active_win_pos_rs::get_active_window;
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
-
     match get_active_window() {
         Ok(active_window) => {
-            println!("active window: {:#?}", active_window);
-            format!("active window: {:#?}", active_window);
-        
-        },
+            // println!("active window: {:#?}", active_window);
+            format!("Hello, {:#?}", active_window)
+        }
         Err(()) => {
-            println!("error occurred while getting the active window");
-            format!("error occurred while getting the active window");
+            // println!("error occurred while getting the active window");
+            format!("Error while fetching window data")
         }
     }
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    // format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
 fn main() {
